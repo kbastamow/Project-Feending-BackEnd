01924c7b93ed75451f1e1156eb47151eb29542f7
joanbaldo <joanbaldo@hotmail.com>
2023-05-22 00:18:15 +0200

    ruta programaciones

---
+++ routes/programaciones.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router()
+const ProgramacionController = require('../controllers/ProgramacionController');
+
+router.post('/create',ProgramacionController.create)
+router.get('/getAllRecintos',ProgramacionController.getAll)
+router.get('/getById/:_id',ProgramacionController.getById)
+router.delete('/delete/:_id',ProgramacionController.delete)
+router.put('/update/:_id',ProgramacionController.update)
+
+
+module.exports = router;
