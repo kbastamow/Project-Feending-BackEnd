dcc32bf91048b59b8e7c4d75583a95d887c55631
joanbaldo <joanbaldo@hotmail.com>
2023-05-22 22:03:24 +0200

    CRUD notificaciones testead y operativo

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ app.use('/sponsors', require('./routes/sponsors'));
 app.use('/recintos', require('./routes/recintos'));
 app.use('/eventos', require('./routes/eventos'));
 app.use('/programaciones', require('./routes/programaciones'));
+app.use('/notificaciones', require('./routes/notificaciones'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
