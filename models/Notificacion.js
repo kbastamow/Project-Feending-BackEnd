67159737cd2681c37a333292e6039c7eed476516
joanbaldo <joanbaldo@hotmail.com>
2023-05-22 21:01:07 +0200

    Creado modelo notificacion para la nueva vista

---
+++ models/Notificacion.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const NotificacionSchema = new mongoose.Schema({
+    idEvento: String,
+    contenido: String,
+    recurrente:  { type: Boolean, default: false },
+    dia: Date,
+    hora: Date  
+  }, { timestamps: true }
+);
+
+const Notificacion = mongoose.model("Notificacion", NotificacionSchema);
+
+module.exports = Notificacion;
